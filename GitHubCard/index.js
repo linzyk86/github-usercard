6e8e6ee055a27e7d0645dca579ea6b1e95f4fbcb
Lindsay W <lwhite5314@gmail.com>
2020-04-15 21:34:16 -0400

    first commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -25,7 +25,7 @@
 */
 
 const followersArray = [];
-
+ 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
